fecc36dd0a25f409e7da720c79c5b6583b794e91
useful <useful.kyo@gmail.com>
2021-01-30 07:25:01 -0800

    configuration added

---
+++ app/config/auth.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    secret: "codetech-training-secret-key",
+    adminDefaultPassword: "blogAdmin",
+    authorDefaultPassword: "blogAuthor",
+    userDefaultPassword: "blogUser"
+}
